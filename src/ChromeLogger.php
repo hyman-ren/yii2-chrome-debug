c35cbe0b51e2218580d1439e62498a76141fade1
hyman <hyman@an2.net>
2020-04-14 21:04:04 +0800

    group

--- src/ChromeLogger.php
+++ src/ChromeLogger.php
@@ -1,9 +1,7 @@
 <?php
 
-
 namespace hyman\debug;
 
-use Yii;
 use yii\log\logger;
 
 /**
@@ -26,48 +24,49 @@ class ChromeLogger extends Logger
      * @return mixed
      * @author    hyman    hyman@an2.net
      */
-    public function log($message, $level, $category = 'application'){
-        if($level == self::LEVEL_PROFILE_BEGIN){
+    public function log($message, $level, $category = 'application')
+    {
+        if ($level == self::LEVEL_PROFILE_BEGIN) {
             self::$timer = microtime(true) * 1000;
         }
 
         $rel = [
-            self::LEVEL_ERROR => 'error',
-            self::LEVEL_WARNING => 'warn',
-            self::LEVEL_INFO => 'info',
-            self::LEVEL_TRACE => 'log',
+            self::LEVEL_ERROR       => 'error',
+            self::LEVEL_WARNING     => 'warn',
+            self::LEVEL_INFO        => 'info',
+            self::LEVEL_TRACE       => 'log',
             self::LEVEL_PROFILE_END => 'log',
         ];
         $func = $rel[$level] ?? '';
-        if(empty($func)){
+        if (empty($func)) {
             return parent::log($message, $level, $category);
         }
         $ext = '';
         $per = '';
-        if($level == self::LEVEL_PROFILE_END){
-            if(in_array($category, ['yii\db\Command::query', 'yii\db\Command::execute'])){
-                self::$sqlCoounter ++;
+        if ($level == self::LEVEL_PROFILE_END) {
+            if (in_array($category, ['yii\db\Command::query', 'yii\db\Command::execute'])) {
+                self::$sqlCoounter++;
                 $per = '第' . self::$sqlCoounter . '条SQL:';
             }
             $ext = ' 耗时' . (microtime(true) * 1000 - self::$timer) . 'ms';
         }
         $message = is_string($message) ? $message : json_encode($message);
-        $message = $per . $message .$ext;
-        $traces = [];
-        $count = 0;
-        $ts = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $message = $per . $message . $ext;
+        $traces  = [];
+        $count   = 0;
+        $ts      = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
         array_pop($ts); // remove the last trace since it would be the entry script, not very useful
         foreach ($ts as $trace) {
             if (isset($trace['file'], $trace['line']) && strpos($trace['file'], YII2_PATH) !== 0) {
                 unset($trace['object'], $trace['args']);
-                $traces[] = $trace['file'] . ' line:' .$trace['line'];
+                $traces[] = $trace['file'] . ' line:' . $trace['line'];
                 if (++$count >= 3) {
                     break;
                 }
             }
         }
 
-        $message .= "\n" .implode("\n",$traces);
+        $message .= "\n" . implode("\n", $traces);
         ChromePhp::$func($message);
         return parent::log($message, $level, $category);
     }
--- src/Module.php
+++ src/Module.php
@@ -3,9 +3,9 @@
 namespace hyman\debug;
 
 use Yii;
+use yii\base\BootstrapInterface;
 use yii\web\Application;
 use yii\web\Controller;
-use yii\base\BootstrapInterface;
 use yii\web\View;
 
 /**
@@ -24,19 +24,20 @@ class Module extends \yii\base\Module implements BootstrapInterface
     {
         $app->on(Application::EVENT_BEFORE_REQUEST, function () use ($app) {
             self::$time = microtime(true) * 1000;
-         	$logger = new ChromeLogger();
-         	$targets = Yii::$app->getLog()->setLogger($logger);
-        	Yii::setLogger($logger);
-        	ChromePhp::groupCollapsed('being');
+            $logger     = new ChromeLogger();
+            $targets    = Yii::$app->getLog()->setLogger($logger);
+            Yii::setLogger($logger);
+            ChromePhp::groupCollapsed('being');
         });
 
         $app->on(Application::EVENT_BEFORE_ACTION, function () use ($app) {
             $controller = $app->controller;
-            $view = $app->getView();
-            $route = $app->requestedRoute;            
+            $view       = $app->getView();
+            $route      = $app->requestedRoute;
             ChromePhp::groupEnd();
             ChromePhp::groupCollapsed($route);
-            $controller->on(Controller::EVENT_BEFORE_ACTION,[$this,'runBeforeAction']);
+            ChromePhp::groupCollapsed('beforRunAction');
+            $controller->on(Controller::EVENT_BEFORE_ACTION, [$this, 'runBeforeAction']);
             $app->getView()->on(View::EVENT_BEFORE_RENDER, [$this, 'runBeforeRender']);
             $app->getView()->on(View::EVENT_BEGIN_PAGE, [$this, 'runBeginPage']);
 
@@ -44,27 +45,27 @@ class Module extends \yii\base\Module implements BootstrapInterface
         $app->on(Application::EVENT_AFTER_REQUEST, function () use ($app) {
             ChromePhp::groupEnd();
             ChromePhp::groupCollapsed('pageInfo');
-            $request = $app->getRequest();
+            $request  = $app->getRequest();
             $response = $app->getResponse();
-            $info = [
+            $info     = [
                 'phpVersion' => PHP_VERSION,
                 'yiiVersion' => Yii::getVersion(),
-                'env' => YII_ENV,
-                'version' => PHP_VERSION,
-                'url' => $request->getAbsoluteUrl(),
-                'ajax' => (int) $request->getIsAjax(),
-                'method' => $request->getMethod(),
-                'ip' => $request->getUserIP(),
-                'time' => $_SERVER['REQUEST_TIME_FLOAT'],
+                'env'        => YII_ENV,
+                'version'    => PHP_VERSION,
+                'url'        => $request->getAbsoluteUrl(),
+                'ajax'       => (int) $request->getIsAjax(),
+                'method'     => $request->getMethod(),
+                'ip'         => $request->getUserIP(),
+                'time'       => $_SERVER['REQUEST_TIME_FLOAT'],
                 'statusCode' => $response->statusCode,
-                'sqlCount' => Yii::getLogger()::$sqlCoounter,
-                'memory' => sprintf('%.3f MB', memory_get_peak_usage() / 1048576),
-                'time' => sprintf("%.2fms",(microtime(true) * 1000 - self::$time)),
+                'sqlCount'   => Yii::getLogger()::$sqlCoounter,
+                'memory'     => sprintf('%.3f MB', memory_get_peak_usage() / 1048576),
+                'time'       => sprintf("%.2fms", (microtime(true) * 1000 - self::$time)),
             ];
 
             foreach ($info as $key => $value) {
-                $value = is_string($value) ? $value : print_r($value,true);
-                ChromePhp::info(sprintf("%s : %s",$key, $value));
+                $value = is_string($value) ? $value : print_r($value, true);
+                ChromePhp::info(sprintf("%s : %s", $key, $value));
             }
 
             ChromePhp::groupEnd();
@@ -76,26 +77,30 @@ class Module extends \yii\base\Module implements BootstrapInterface
      *
      * @author    hyman    hyman@an2.net
      */
-    public function runBeforeAction(){
-           ChromePhp::group('action');
+    public function runBeforeAction()
+    {
+        ChromePhp::groupEnd();
+        ChromePhp::groupCollapsed('action');
     }
 
     /**
      *
      * @author    hyman    hyman@an2.net
      */
-    protected function runBeforeRender(){
+    protected function runBeforeRender()
+    {
         ChromePhp::groupEnd();
-        ChromePhp::group('render');
+        ChromePhp::groupCollapsed('render');
     }
 
     /**
      *
      * @author    hyman    hyman@an2.net
      */
-    protected function runBeginPage(){
+    protected function runBeginPage()
+    {
         ChromePhp::groupEnd();
-        ChromePhp::group('page');
+        ChromePhp::groupCollapsed('page');
     }
 
 }
